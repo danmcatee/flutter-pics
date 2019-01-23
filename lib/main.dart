f90a4988da10c1c24e2b7267e197499fc3d3a59f
Dan McAtee <info@danmcatee.com>
2019-01-23 01:43:04 +0100

    Add AppBar and FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    home: Text('Hello World'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('hi there');
+        },
+        child: Icon(Icons.add),
+      ),
+    ),
   );
 
   runApp(app);
