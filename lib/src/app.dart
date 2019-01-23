67b361e86b60f70353afb0825855f477d45faf42
Dan McAtee <info@danmcatee.com>
2019-01-23 04:00:01 +0100

    Render out Images

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
 import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -14,12 +15,11 @@ class AppState extends State<App> {
   List<ImageModel> images = [];
 
   void fetchImage() async {
-    setState(() {
-      counter++;
-    });
+    counter++;
     var response =
         await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
+
     setState(() {
       images.add(imageModel);
     });
@@ -28,13 +28,13 @@ class AppState extends State<App> {
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
         appBar: AppBar(
           title: Text('Lets see some images!'),
-        ),
-        body: Text('$counter'),
-        floatingActionButton: FloatingActionButton(
-          onPressed: fetchImage,
-          child: Icon(Icons.add),
         ),
       ),
     );
